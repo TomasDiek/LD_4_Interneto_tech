6564f523254f100805e1116c3ba73cc8d87e420d
TomasDiek <t.diekantas@gmail.com>
2023-04-14 21:27:14 +0300

    Add project files.

---
+++ LD_4_Interneto_tech/Controllers/CityControler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LD_4_Interneto_tech.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityControler : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Kaunas", "Vilnius" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Kaunas";
+        }
+    }
+}
