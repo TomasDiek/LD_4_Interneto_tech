73b25e8ac8c361c2c468d3d1cd4bbd742fc521ef
TomasDiek <t.diekantas@gmail.com>
2023-04-23 18:15:58 +0300

    minor

--- LD_4_Interneto_tech/Interfaces/IPhotoService.cs
+++ LD_4_Interneto_tech/Interfaces/IPhotoService.cs
@@ -7,8 +7,6 @@ namespace LD_4_Interneto_tech.Interfaces
     public interface IPhotoService
     {
          Task<ImageUploadResult> UploadPhotoAsync(IFormFile photo);
-
-         //will add onre more method for deleting the photo
          Task<DeletionResult> DeletePhotoAsync(string publicId);
         
     }
